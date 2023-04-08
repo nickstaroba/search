6856f75050064e9f250d4734f75b874df045e8b2
Nick Staroba <nick@nickstaroba.com>
2023-04-08 11:58:23 -0400

    feat: adding wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
 import { Typography } from "@mui/material";
+import { Link, Route, Switch } from "wouter";
 
-export function App() {
-  return <Typography variant={"h1"}>Search</Typography>;
-}
+export const App = () => (
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a className="link">Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="link">About</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <Switch>
+        <Route path="/">
+          <Typography variant={"h1"}>Search</Typography>
+        </Route>
+        <Route path="/about">
+          <Typography variant={"h1"}>About Us</Typography>
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Router } from "wouter";
 
 import { App } from "./App";
 import theme from "./theme";
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Router base={"/search"}>
+        <App />
+      </Router>
     </ThemeProvider>
   </React.StrictMode>
 );
